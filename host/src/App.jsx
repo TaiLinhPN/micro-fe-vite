76e07aff7c74a772e9c73167c4f18fb8ce2700bd
Linh Nguyen Bui Tai <linh.nguyenbuitai>
2023-07-04 10:13:12 +0700

    setup share component

---
+++ host/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Button from "remoteApp/Button";
+function App() {
+  return (
+    <>
+      <h1>Host</h1>
+      <div className="card">
+        <Button/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ remote/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Button from './Button'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Remote app</h1>
+        <Button/>
+      </div>
+      <div className="card">
+      </div>
+    </>
+  )
+}
+
+export default App
